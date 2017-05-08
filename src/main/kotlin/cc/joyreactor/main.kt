bd2d54164ba7345e429b377135b4ff47c6410b96
y2k <itwisterlx@gmail.com>
2017-05-08 10:15:02 +0300

    profile + refactoring

--- src/main/kotlin/cc/joyreactor/main.kt
+++ src/main/kotlin/cc/joyreactor/main.kt
@@ -1,38 +1,41 @@
 package cc.joyreactor
 
 import cc.joyreactor.core.*
+import cc.joyreactor.core.JoyReactor.postWithComments
 import com.google.gson.Gson
 import org.jsoup.Jsoup
 import spark.Spark.post
+import java.io.InputStream
+import java.lang.System.getProperty
 import javax.servlet.MultipartConfigElement
 
 fun main(args: Array<String>) {
-    post("/posts", { request, _ ->
-        request.attribute("org.eclipse.jetty.multipartConfig", MultipartConfigElement("/temp"))
-        val html = request.raw().getPart("html").inputStream.bufferedReader().readText()
-        Domain.parse(html)
-    }, Gson()::toJson)
+    mutlipart("/posts") { Domain.getPostsWithNext(it) }
+    mutlipart("/post") { Domain.getPostWithTopComments(it) }
+    mutlipart("/profile") { Domain.getProfile(it) }
+}
 
-    post("/post", { request, _ ->
-        request.attribute("org.eclipse.jetty.multipartConfig", MultipartConfigElement("/temp"))
-        val html = request.raw().getPart("html").inputStream.bufferedReader().readText()
-        Domain.parsePostWithTopComments(html)
+fun mutlipart(path: String, handler: (InputStream) -> Any) {
+    post(path, { request, _ ->
+        request.attribute("org.eclipse.jetty.multipartConfig", MultipartConfigElement(getProperty("java.io.tmpdir")))
+        request.raw().getPart("html").inputStream.use { handler(it) }
     }, Gson()::toJson)
 }
 
 object Domain {
 
-    fun parse(html: String): Posts =
-        Jsoup.parse(html)
-            .let { doc ->
-                Posts(
-                    Parsers.parsePostsForTag(doc),
-                    Parsers.parseNewPageNumber(doc))
-            }
+    fun getProfile(stream: InputStream): Profile =
+        Parsers.profile(stream.html())
 
-    fun parsePostWithTopComments(html: String): Post =
-        Jsoup.parse(html)
-            .let { doc ->
-                JoyReactor.postWithComments(doc, TopComments(10))
-            }
+    fun getPostsWithNext(stream: InputStream): Posts =
+        stream.html().let { doc ->
+            Posts(
+                Parsers.parsePostsForTag(doc),
+                Parsers.parseNewPageNumber(doc))
+        }
+
+    fun getPostWithTopComments(stream: InputStream): Post =
+        postWithComments(stream.html(), TopComments(10))
+
+    private fun InputStream.html() = Jsoup.parse(bufferedReader().readText())
 }
